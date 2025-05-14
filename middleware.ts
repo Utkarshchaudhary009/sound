861b2a38e2277196643c301860facb1a74667231
utkarsh chaudhary <utkarshchaudhary426@gmail.com>
2025-05-15 04:24:43 +0530

    middleware handled

--- middleware.ts
+++ middleware.ts
@@ -1,15 +1,16 @@
-import { authMiddleware } from "@clerk/nextjs"
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/search", "/category(.*)"],
+const isPublicRoute = createRouteMatcher(["/", "/search", "/category(.*)","/api/(.*)"])
 
-  // Routes that can always be accessed, and have
-  // no authentication information
-  ignoredRoutes: ["/api/webhook"],
+export default clerkMiddleware(async (auth, req) => {
+  if (!isPublicRoute(req)) await auth.protect()
 })
 
 export const config = {
-  // Matcher ignoring _next/static, _next/image, favicon.ico, etc
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
 }
